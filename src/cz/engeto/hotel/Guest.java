856866d2c675eff3beb606546bddd3cc0724d120
kryptoraptor <kryptoraptorblogcz@gmail.com>
2025-10-09 18:41:32 +0200

    Guest class
    Created a class to store guest data

---
+++ src/cz/engeto/hotel/Guest.java
@@ -0,0 +1,40 @@
+package cz.engeto.hotel;
+
+import java.time.LocalDate;
+
+public class Guest {
+    public String firstName;
+    public String lastName;
+    public LocalDate birthDate;
+
+    public Guest(String firstName, String lastName, LocalDate birthDate)
+    {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.birthDate = birthDate;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public LocalDate getBirthDate() {
+        return birthDate;
+    }
+
+    public void setBirthDate(LocalDate birthDate) {
+        this.birthDate = birthDate;
+    }
+}
